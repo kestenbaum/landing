5fc270a68cfea34388773599b194a2c7b012f08e
Oleksii Vovnenko <kespa71for@gmail.com>
2024-12-27 04:42:39 +0100

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
-console.log("init app")
+const burgerMenu = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const headerMenuLinks = document.querySelectorAll('.header__link');
+
+
+burgerMenu.onclick = () => {
+    headerMenu.classList.toggle('active');
+}
+
+headerMenuLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        headerMenu.classList.remove('active');
+    })
+})
