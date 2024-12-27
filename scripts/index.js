d5cc7120bda81cf787b0ab8f5ee587f6ac54340b
Oleksii Vovnenko <kespa71for@gmail.com>
2024-12-27 17:59:41 +0100

    fix styles

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,18 @@ const burgerMenu = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
 const headerMenuLinks = document.querySelectorAll('.header__link');
 
+document.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        const columns = section.querySelector('.wrapper');
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+            columns.classList.add('shrink');
+        } else {
+            columns.classList.remove('shrink');
+        }
+    });
+});
 
 burgerMenu.onclick = () => {
     headerMenu.classList.toggle('active');
